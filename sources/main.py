e6fd8fab48353d53c901335d02ea4e0bf9745d01
Stepan Oksanichenko <zel.gray@gmail.com>
2024-02-12 21:46:46 +0200

    - Fixer `vxtwitter` is replaced by `fxtwitter` - It's added processing `x.com` (even when an embed link contains `twitter.com`)

--- sources/main.py
+++ sources/main.py
@@ -2,7 +2,12 @@
 import asyncio
 import logging
 import os
+from collections import defaultdict
 from copy import copy
+from dataclasses import dataclass
+from tldextract import extract
+from tldextract.tldextract import ExtractResult
+from typing import Optional
 from urllib.parse import urlparse, ParseResult
 
 import discord
@@ -14,6 +19,13 @@ intents.message_content = True
 bot = Bot(command_prefix="", intents=intents)
 
 logger = logging.getLogger('discord')
+
+
+@dataclass
+class DomainFixer:
+    fixer: str
+    second_fixer: Optional[str] = None
+    second_domain: Optional[str] = None
 
 
 @bot.tree.command(
@@ -29,35 +41,69 @@ async def ping(interaction):
     await interaction.response.send_message('pong')
 
 
-def replace_domain(url: ParseResult):
+def fix_url(msg_content: str, url: str) -> tuple[str, str]:
     """
-    Replace an original domain by a fixer
+    Fix a URL by replacing an original domain by a fixer
+    :param msg_content: content of an original message
     :param url: an original parsed URL
-    :return: a fixed parsed URL
+    :return: a fixed URL
     """
-    domains = {
-        'reddit.com': 'rxddit.com',
-        'www.reddit.com': 'rxddit.com',
-        'tiktok.com': 'vxtiktok.com',
-        'www.tiktok.com': 'vxtiktok.com',
-        'vm.tiktok.com': 'vm.vxtiktok.com',
-        'www.vm.tiktok.com': 'vm.vxtiktok.com',
-        'twitter.com': 'vxtwitter.com',
-        'www.twitter.com': 'vxtwitter.com',
-        'instagram.com': 'ddinstagram.com',
-        'www.instagram.com': 'ddinstagram.com',
-    }
-    for key, value in domains.items():
-        if url.netloc == key:
-            return ParseResult(
-                url.scheme,
-                netloc=value,
-                path=url.path,
-                query=url.query,
-                params=url.params,
-                fragment=url.fragment,
-            )
-    return url
+    domains = defaultdict(lambda: DomainFixer(
+        fixer='',
+    ), **{
+        'reddit.com': DomainFixer(
+            fixer='rxddit',
+        ),
+        'tiktok.com': DomainFixer(
+            fixer='vxtiktok',
+        ),
+        'twitter.com': DomainFixer(
+            fixer='fxtwitter',
+            second_fixer='fixupx',
+            second_domain='x',
+        ),
+        'x.com': DomainFixer(
+            fixer='fixupx',
+            second_fixer='fxtwitter',
+            second_domain='twitter',
+        ),
+    })
+    parsed_url = urlparse(url)
+    parsed_domain = extract(parsed_url.netloc)
+    fixer = domains[parsed_domain.registered_domain]
+    if url in msg_content:
+        fixed_domain = fixer.fixer
+        replaced_url = url
+    else:
+        fixed_domain = fixer.second_fixer
+        replaced_url = ParseResult(
+            parsed_url.scheme,
+            netloc=ExtractResult(
+                subdomain=parsed_domain.subdomain,
+                domain=fixer.second_domain,
+                suffix=parsed_domain.suffix,
+                is_private=parsed_domain.is_private,
+            ).fqdn,
+            path=parsed_url.path,
+            query=parsed_url.query,
+            params=parsed_url.params,
+            fragment=parsed_url.fragment,
+        ).geturl()
+    if not fixer.fixer:
+        return url, url
+    return replaced_url, ParseResult(
+        parsed_url.scheme,
+        netloc=ExtractResult(
+            subdomain=parsed_domain.subdomain,
+            domain=fixed_domain,
+            suffix=parsed_domain.suffix,
+            is_private=parsed_domain.is_private,
+        ).fqdn,
+        path=parsed_url.path,
+        query=parsed_url.query,
+        params=parsed_url.params,
+        fragment=parsed_url.fragment,
+    ).geturl()
 
 
 @bot.event
@@ -77,15 +123,12 @@ async def on_message(message: discord.Message):
     if not message.embeds:
         logger.info('The message does not contain embeds')
         return
-    processed_urls = (
-        replace_domain(urlparse(embed.url)) for embed in message.embeds
+    final_urls = dict(
+        fix_url(
+            msg_content=message.content,
+            url=embed.url,
+        ) for embed in message.embeds
     )
-    final_urls = {
-        embed.url: processed_url.geturl() for processed_url, embed in zip(
-            processed_urls,
-            message.embeds
-        )
-    }
     content = copy(message.content)
     for origin_url, final_url in final_urls.items():
         content = content.replace(origin_url, final_url)
